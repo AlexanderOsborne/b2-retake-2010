b936ded8f7f6f8452a1d7f4f50aec1d790553245
awosborn <alanturing@example.coclear>
2021-01-20 09:18:10 -0700

    Set up DB

---
+++ spec/models/airline_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Airline, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name}
+  end
+
+  describe 'relationships' do
+    it {should have_many :flights}
+  end
+
+  describe 'instance methods' do
+  end
+end
